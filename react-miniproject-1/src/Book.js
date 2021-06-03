85a30adb6e24a917cd285efe5d44c836dd6b77a3
Harsh Solanki <harsh.solanki2105@gmail.com>
2021-06-03 17:33:37 +0530

    hands-on

---
+++ react-miniproject-1/src/Book.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Book = ({ img, title, author }) => {
+  // attribute, eventHandler
+  // onClick, onMouseOver
+  const clickHandler = (e) => {
+    console.log(e)
+    console.log(e.target)
+    alert(title)
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
+  return (
+    <article
+      className='book'
+      onMouseOver={() => {
+        console.log(title)
+      }}
+    >
+      <img src={img} alt='' />
+      <h1 onClick={() => console.log(title)}>{title}</h1>
+      <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        Alert Example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        See Console
+      </button>
+    </article>
+  )
+}
+
+export default Book
---
+++ react-miniproject-1/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+// CSS
+import './index.css'
+
+import { data } from './books'
+import SpecificBook from './Book'
+import { greeting } from './testing/testing'
+
+function BookList() {
+  console.log(greeting)
+  return (
+    <section className='booklist'>
+      {data.map((book) => {
+        return <SpecificBook key={book.id} {...book} />
+      })}
+    </section>
+  )
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
